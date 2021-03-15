68b94b7cb53d66560db46dfd4de5c411115f88bb
Kozmin Jurii <Koriolan2@gmail.com>
2021-03-16 00:34:07 +0200

    20210316

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
-import  ModalWindow from './modules/modalWindow.js';
-import  GotoScrolling from './modules/gotoScrolling.js';
-import  Slider from './modules/slider.js';
+import ModalWindow from './modules/modalWindow.js';
+import Slider from './modules/slider.js';
+// import  GotoScrolling from './modules/gotoScrolling.js';
+
 
 // модальное окно
 const modal = {
     overlay: '.modal-overlay',
     modal: '.modal-callback',
     openBtn: '.header .callback-btn',
-    closeBtn: '.modal-close'
+    closeBtn: '.modal-close',
+    animation: {
+        typeOpen: 'animate__bounceIn',
+        typeExit: 'animate__fadeOutUpBig'
+    }
 };
 const modalWindow = new ModalWindow(modal);
 
 modalWindow.init();
 
-// скроллинг
-const goto = {
-    menu: '.top-menu'
-};
-
-const gotoScrolling = new GotoScrolling(goto);
-
-gotoScrolling.init();
-
 
 const s = {
     slideWrap: '.top-slider',
-    slideElem: '.item'
+    slideElem: '.item',
+    time: 3000,
+    animationImage: {
+        slideOpen:'animate__fadeIn',
+        slideClose:'animate__fadeOut'
+    }
 };
 
 const slider = new Slider(s);
--- src/modules/modalWindow.js
+++ src/modules/modalWindow.js
@@ -1,14 +1,25 @@
 'use strict';
 
 class ModalWindow {
-    constructor({modal, overlay, openBtn, closeBtn}) {
+    constructor({
+        modal, 
+        overlay, 
+        openBtn, 
+        closeBtn,
+        animation = {
+            typeOpen: 'animate__backInDown',
+            typeExit: 'animate__backOutDown',
+        }
+    }) {
         this.modal = document.querySelector(modal);
         this.overlay = document.querySelector(overlay);
         this.openBtn = document.querySelector(openBtn);
         this.closeBtn = document.querySelector(closeBtn);
+        this.animation = animation;
     }
 
     init() {
+        this.setBasicStyles();
         this.controlButtons();        
     }
 
@@ -17,18 +28,61 @@ class ModalWindow {
         this.overlay.addEventListener('click', this.closeModal.bind(this));
         this.closeBtn.addEventListener('click', this.closeModal.bind(this));
     }
+    
+    openOverlay() {
+        this.overlay.style.display = 'block';
+        return true;
+    }
 
+    closeOverlay() {
+        this.overlay.style.display = 'none';
+        return true;
+    }
+
+    openMod() {
+        this.modal.style.display = 'block';
+        this.modal.classList.add('animate__animated', this.animation.typeOpen);
+        return true;   
+    } 
+
+    closeMod() {
+        this.modal.classList.remove(this.animation.typeOpen);
+        this.modal.classList.add(this.animation.typeExit);
+        return true;
+    }
+    
     openModal() {
-       this.overlay.style.display = 'block'; 
-       this.modal.style.display = 'block'; 
-    //    this.modal.classList.add('animate__animated', 'animate__backInDown');
+        if(this.openOverlay()) {
+            setTimeout(this.openMod.bind(this), this.animation.speed);
+        }
+       
     }
 
     closeModal() {
-        this.overlay.style.display = 'none';
-        this.modal.style.display = 'none';  
-        // this.modal.classList.add('animate__animated', 'animate__backOutDown');
+       if(this.closeMod()) {
+           setTimeout(this.closeOverlay.bind(this),300);
+            
+           if (this.closeOverlay) {
+                setTimeout(this.resetStyles.bind(this), 600);
+            }
+        }
     }
-}
+
+    resetStyles() {
+        this.modal.style.display = 'none';
+        this.modal.classList.remove('animate__animated', this.animation.typeOpen, this.animation.typeExit);
+    } 
+
+    setBasicStyles() {
+        const urlLibrary = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css',
+              styleAnimate = document.createElement('link');
+
+        styleAnimate.setAttribute('rel', 'stylesheet'),
+        styleAnimate.setAttribute('href', urlLibrary);
+        
+        document.head.append(styleAnimate);
+    }
+
+} 
 
 export default ModalWindow;
